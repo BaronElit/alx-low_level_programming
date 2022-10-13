f8aa9ae73fe5a0b16ff332abe6fd16fbe28fa396
BaronElit <tiburcehoundetet@gmail.com>
2022-10-13 12:43:53 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,17 @@
 #include <string.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
 {
-char echot[60];
-strcpy(echot, "\"Programming is like building a multilingual puzzle\n");
-puts(echot);
-return (0);
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
 }
