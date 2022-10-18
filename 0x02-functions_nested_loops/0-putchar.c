cd4c9d1942c9f193d1ee164709264f11e595e19b
BaronElit <tiburcehoundetet@gmail.com>
2022-10-18 07:09:32 -0700

    answer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }
