37127bba81e1641fb059fcb209917138bf0db493
BaronElit <tiburcehoundetet@gmail.com>
2022-10-20 15:21:37 -0700

    answer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,16 +4,11 @@
 *
 *Return: returns nothing
 */
-void print_most_numbers(void)
+void print_numbers(void)
 {
-int number;
-for (number = 48; number < 58; number++)
-{
-if ((number == 50) || (number == 52))
-{
-continue;
+  char c;
+  for(c = '0' ; c <='9'; c++)
+    _putchar(c);
+  _putchar('\n');
 }
-_putchar(number);
-}
-_putchar(10);
-}
+  
